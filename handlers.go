1f09673dea4906ca895bdcdf68a5e5e165f589a8
Richard Kille <rjkille@gmail.com>
2018-02-02 13:01:36 +0000

    Names can be fetched using full form name

--- handlers.go
+++ handlers.go
@@ -9,8 +9,15 @@ func formHandler(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
 
 	fmt.Println("Form:",r.Form)
-	fmt.Println("People:", r.Form["people"])
+	fmt.Println("People:", r.Form["people[][firstname]"])
 	fmt.Println("PostForm:", r.PostForm)
+
+	firstnames := r.Form["people[][firstname]"]
+	surnames := r.Form["people[][surname]"]
+
+	for i,v := range firstnames {
+		fmt.Printf("Person %d: %s %s\r\n", i, v, surnames[i])
+	}
 
 
 	http.ServeFile(w, r, "markup.html")
