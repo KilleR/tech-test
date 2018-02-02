e73adbedd7f3d3d4f0f5c413128b79aadda6ee9b
Richard Kille <rjkille@gmail.com>
2018-02-02 12:56:39 +0000

    Test for form parsing - go doesn't like php-style name markup

--- handlers.go
+++ handlers.go
@@ -2,8 +2,16 @@ package main
 
 import (
 	"net/http"
+	"fmt"
 )
 
 func formHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+
+	fmt.Println("Form:",r.Form)
+	fmt.Println("People:", r.Form["people"])
+	fmt.Println("PostForm:", r.PostForm)
+
+
 	http.ServeFile(w, r, "markup.html")
 }
