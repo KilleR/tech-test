fcbb1352820091c5043fcbc173d576c1c2ee56b9
Richard Kille <rjkille@gmail.com>
2018-02-02 13:56:29 +0000

    Added file handling to DB struct
    removed old storage, as it is now a placeholder for get and set functions only

--- db.go
+++ db.go
@@ -1,14 +1,44 @@
 package main
 
+import (
+	"encoding/json"
+	"log"
+	"os"
+	"io/ioutil"
+)
+
 type PeopleDb struct {
-	people []Person
 }
 
 func (db *PeopleDb) GetAll() (p []Person) {
-	p = db.people
+	// fetch contents of db file
+	dbtext, err := ioutil.ReadFile("db")
+	if err != nil {
+		log.Println("Error reading from DB file:", err)
+		return // TODO: Should have error trapping here
+	}
+
+	// parse db json into output
+	err = json.Unmarshal(dbtext, &p)
+	if err != nil {
+		log.Println("Error parsing db content:",err)
+		return
+	}
 	return
 }
 
 func (db *PeopleDb) SetAll(p []Person) {
-	db.people = p
+	dbtext, err := json.Marshal(p)
+	if err != nil {
+		log.Println("Error marshalling DB content:", err)
+		return
+	}
+	// open DB file handler - create if not present, truncate before writing
+	file, err := os.OpenFile("db", os.O_CREATE|os.O_RDWR|os.O_TRUNC, 0644)
+
+	// write data to file
+	_, err = file.Write(dbtext)
+	if err != nil {
+		log.Println("Error writing to DB file:", err)
+	}
 }
--- main.go
+++ main.go
@@ -6,13 +6,6 @@ var DB *PeopleDb
 
 func init() {
 	DB = new(PeopleDb)
-	DB.people = []Person{
-		Person{"Jeff", "Stelling"},
-		Person{"Chris", "Kamara"},
-		Person{"Alex", "Hammond"},
-		Person{"Jim", "White"},
-		Person{"Natalie", "Sawyer"},
-	}
 }
 
 func main() {
