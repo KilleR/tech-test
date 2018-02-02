f46b58af01a5152ef07cc1f04f87885f2317f107
Richard Kille <rjkille@gmail.com>
2018-02-02 13:33:24 +0000

    added trap for blank form data

--- handlers.go
+++ handlers.go
@@ -13,13 +13,16 @@ func formHandler(w http.ResponseWriter, r *http.Request) {
 	firstnames := r.Form["people[][firstname]"]
 	surnames := r.Form["people[][surname]"]
 
-	// collect form data for people
-	var newPeople []Person
-	for i,v := range firstnames {
-		newPeople = append(newPeople, Person{v, surnames[i]})
+	// only handle form data if there is form data present
+	if len(firstnames) > 0 && len(surnames) > 0 {
+		// collect form data for people
+		var newPeople []Person
+		for i, v := range firstnames {
+			newPeople = append(newPeople, Person{v, surnames[i]})
+		}
+		// overwrite stored people
+		people = newPeople
 	}
-	// overwrite stored people
-	people = newPeople
 
 	t, err := template.ParseFiles("tmpl/markup.html")
 	if err != nil {
